e764c950c6d90074cb44aa017aea77705f534440
ramseyFHSU <ramsey.fhsu@gmail.com>
2021-11-30 16:58:33 -0600

    Adding Fe-meal-tracker

---
+++ fe-meal-tracker/src/app/app-routing.module.ts
@@ -0,0 +1,31 @@
+import { ShoppingListPageComponent } from './shopping-list-page/shopping-list-page.component';
+import { AddIngredientPageComponent } from './add-ingredient-page/add-ingredient-page.component';
+import { RecipeSearchPageComponent } from './recipe-search-page/recipe-search-page.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomePageComponent,
+  },
+  {
+    path: 'recipes',
+    component: RecipeSearchPageComponent,
+  },
+  {
+    path: 'add-ingredient',
+    component: AddIngredientPageComponent,
+  },
+  {
+    path: 'shopping-list',
+    component: ShoppingListPageComponent,
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
---
+++ fe-meal-tracker/src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { IngredientsListComponent } from './ingredients-list/ingredients-list.component';
+import { MealsListComponent } from './meals-list/meals-list.component';
+import { RecipeSearchPageComponent } from './recipe-search-page/recipe-search-page.component';
+import { RecipeSearchResultsListComponent } from './recipe-search-results-list/recipe-search-results-list.component';
+import { ShoppingListPageComponent } from './shopping-list-page/shopping-list-page.component';
+import { AddIngredientPageComponent } from './add-ingredient-page/add-ingredient-page.component';
+import { SmallXComponent } from './small-x/small-x.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomePageComponent,
+    IngredientsListComponent,
+    MealsListComponent,
+    RecipeSearchPageComponent,
+    RecipeSearchResultsListComponent,
+    ShoppingListPageComponent,
+    AddIngredientPageComponent,
+    SmallXComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
